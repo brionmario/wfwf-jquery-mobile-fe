028e9d8cbb0458ebb858702fa1a0cad6bf54c213
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-10 23:00:49 +0530

    chore(header): modify header to match styleguide

--- src/components/header.php
+++ src/components/header.php
@@ -2,4 +2,6 @@
   <a href="#sidepanel">
     <i class="fa fa-bars"></i>
   </a>
+  <img src="assets/img/logos/logo-mini-white-on-black.png" class="mini-logo"/>
+  <button class="btn btn-primary text-uppercase login-btn ui-btn ui-corner-all">Login</button>
 </div>
