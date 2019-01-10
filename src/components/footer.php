88773ca5afde34ca91adc9642674a171e00a09d6
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-11 04:22:37 +0530

    chore(components): add accordion to footer

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,18 +1,58 @@
 <div data-role="footer" class="footer">
-  <ul class="footer-icons">
-    <li><a href="https://www.facebook.com/apareciumlabs/" target="_blank"><i class="fa fa-facebook-square"></i></a>
-    </li>
-    <li><a href="https://twitter.com/apareciumlabs" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
-    <li><a href="https://github.com/apareciumlabs" target="_blank"><i class="fa fa-github-square"></i></a></li>
-  </ul>
-  <div class="copyright">
-    <p>&copy; Copyright
-      <script language="JavaScript" type="text/javascript">
-        document.write(getYear())
+  <div class="navigate-top">
+    <i class="fa fa-angle-double-up"></i>
+  </div>
+  <div class="footer-content">
+    <div class="accordion">
+      <div data-role="collapsible-set" data-iconpos="right" data-inset="false" data-corners="false">
+        <div data-role="collapsible">
+          <h3>Specials</h3>
+          <ul data-role="listview" data-inset="false">
+            <li>Coupon Codes</li>
+            <li>Student Discounts</li>
+            <li>Poster Signings</li>
+          </ul>
+        </div>
+      </div>
+      <div data-role="collapsible-set" data-iconpos="right" data-inset="false" data-corners="false">
+        <div data-role="collapsible">
+          <h3>Join Us</h3>
+          <ul data-role="listview" data-inset="false">
+            <li>About</li>
+            <li>Meet Our Team</li>
+            <li>Join The Commitee</li>
+          </ul>
+        </div>
+      </div>
+      <div data-role="collapsible-set" data-iconpos="right" data-inset="false" data-corners="false">
+        <div data-role="collapsible">
+          <h3>Help& Support</h3>
+          <ul data-role="listview" data-inset="false">
+            <li>Contact</li>
+            <li>Help With Bookings</li>
+          </ul>
+        </div>
+      </div>
+      <div data-role="collapsible-set" data-iconpos="right" data-inset="false" data-corners="false">
+        <div data-role="collapsible">
+          <h3>Information</h3>
+          <ul data-role="listview" data-inset="false">
+            <li>Terms & Conditions</li>
+            <li>Cookie Consent</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      <p>&copy; Copyright
+        <script language="JavaScript" type="text/javascript">
+          document.write(getYear())
 
-      </script>
-    </p>
-    <img src="assets/img/logo_aparecium_fox_only.svg" width="20px" height="20px" />
-    <p>Aparecium Labs. All rights reserved.</p>
+        </script>
+      </p>
+      <img src="assets/img/logo_aparecium_fox_only.svg" width="20px" height="20px" />
+      <p>Aparecium Labs. All rights reserved.</p>
+    </div>
   </div>
 </div>
