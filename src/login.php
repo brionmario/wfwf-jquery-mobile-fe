df81052833ada54a1bafdb97cf1e841ff9fc9085
ThisuraSagara <thisuras4@gmail.com>
2019-01-11 02:15:30 +0530

    add basic templte to login

---
+++ src/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="author" content="Brion Silva">
+  <title>Login</title>
+  <!-- Favicon Package -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
+  <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
+  <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+  <!-- inject:css -->
+  <!-- endinject -->
+</head>
+
+<body>
+<div data-role="page">
+  <div role="main" class="overlay ui-content main-content">
+
+  </div><!-- /content -->
+</div><!-- page -->
+<!-- inject:js -->
+<!-- endinject -->
+</body>
+</html>
