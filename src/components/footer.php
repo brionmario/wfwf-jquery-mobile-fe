658d87616de5ba25f56fb56d988fe2e17970aee2
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-11 07:56:55 +0530

    chore(components): work on footer styling and responsiveness

--- src/components/footer.php
+++ src/components/footer.php
@@ -56,47 +56,51 @@
         </div>
       </div>
     </div><!-- /accordion -->
-    <div class="footer-content-grid ui-grid-c my-breakpoint">
-	    <div class="ui-block-a">
-        <h6>Specials</h6>
-        <ul>
-          <li>Coupon Codes</li>
-          <li>Student Discounts</li>
-          <li>Poster Signings</li>
-        </ul>
-      </div>
-	    <div class="ui-block-b">
-        <h6>Join Us</h6>
-        <ul>
-          <li>About</li>
-          <li>Meet Our Team</li>
-          <li>Join The Commitee</li>
-        </ul>
-      </div>
-	    <div class="ui-block-c">
-        <h6>Help& Support</h6>
-        <ul>
-          <li>Contact</li>
-          <li>Help With Bookings</li>
-        </ul>
-      </div>
-      <div class="ui-block-d">
-        <h6>Information</h6>
-        <ul>
-          <li>Terms & Conditions</li>
-          <li>Cookie Consent</li>
-          <li>Privacy Policy</li>
-        </ul>
+    <div class="footer-content-grid">
+      <div class="ui-grid-c breakpoint">
+        <div class="footer-content-block ui-block-a">
+          <h5>Specials</h5>
+          <ul>
+            <li>Coupon Codes</li>
+            <li>Student Discounts</li>
+            <li>Poster Signings</li>
+          </ul>
+        </div>
+        <div class="footer-content-block ui-block-b">
+          <h5>Join Us</h5>
+          <ul>
+            <li>About</li>
+            <li>Meet Our Team</li>
+            <li>Join The Commitee</li>
+          </ul>
+        </div>
+        <div class="footer-content-block ui-block-c">
+          <h5>Help& Support</h5>
+          <ul>
+            <li>Contact</li>
+            <li>Help With Bookings</li>
+          </ul>
+        </div>
+        <div class="footer-content-block ui-block-d">
+          <h5>Information</h5>
+          <ul>
+            <li>Terms & Conditions</li>
+            <li>Cookie Consent</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
       </div>
     </div><!-- /footer-content-grid -->
-    <div class="copyright">
+  </div>
+  <div class="copyright">
+    <a rel="external" target="_blank" href="http://apareciumlabs.com/">
       <p>&copy; Copyright
-        <script type="text/javascript">
-          document.write(new Date().getFullYear())
-        </script>
+      <script type="text/javascript">
+        document.write(new Date().getFullYear())
+      </script>
       </p>
       <img src="assets/img/logo_aparecium_fox_only.svg" width="20px" height="20px" />
       <p>Aparecium Labs. All rights reserved.</p>
-    </div>
-  </div>
+    </a>
+  </div><!-- /copyright -->
 </div>
