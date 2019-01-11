c14cd7695507cc01ea967f473c7958868e4d58ea
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-11 21:12:16 +0530

    refactor(pages): rename events desc file :recycle:

--- src/event-description.php
+++ src/event-description.php
@@ -8,14 +8,18 @@
   <meta name="theme-color" content="#ffffff">
   <meta name="author" content="Brion Silva">
   <title>jQuery Mobile Seed by Aparecium Labs</title>
+ 
   <!-- Favicon Package -->
   <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
   <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
   <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+
   <!-- inject:css -->
-  
+  <!-- endinject -->
+
+  <!-- inject:js -->
   <!-- endinject -->
 </head>
 
