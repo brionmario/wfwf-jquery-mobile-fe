673614171338b08ebd0ee2951f83ee515610c4a8
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-11 18:33:40 +0530

    chore(components): add avatar to header

--- src/components/header.php
+++ src/components/header.php
@@ -4,4 +4,7 @@
   </a>
   <img src="assets/img/logos/logo-mini-white-on-black.png" class="mini-logo"/>
   <button class="btn btn-primary text-uppercase login-btn ui-btn ui-corner-all">Login</button>
+  <div class="avatar-container">
+    <img class="avatar" src="assets/img/avatars/avatar.svg" />
+  </div>
 </div>
