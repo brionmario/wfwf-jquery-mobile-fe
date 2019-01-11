dc8eea4cb32e7095bad0e86dd85cde4e1cd9532a
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-11 06:22:05 +0530

    chore(components): add basic media query template to footer

--- src/components/footer.php
+++ src/components/footer.php
@@ -55,7 +55,40 @@
           </ul>
         </div>
       </div>
-    </div>
+    </div><!-- /accordion -->
+    <div class="footer-content-grid ui-grid-c my-breakpoint">
+	    <div class="ui-block-a">
+        <h6>Specials</h6>
+        <ul>
+          <li>Coupon Codes</li>
+          <li>Student Discounts</li>
+          <li>Poster Signings</li>
+        </ul>
+      </div>
+	    <div class="ui-block-b">
+        <h6>Join Us</h6>
+        <ul>
+          <li>About</li>
+          <li>Meet Our Team</li>
+          <li>Join The Commitee</li>
+        </ul>
+      </div>
+	    <div class="ui-block-c">
+        <h6>Help& Support</h6>
+        <ul>
+          <li>Contact</li>
+          <li>Help With Bookings</li>
+        </ul>
+      </div>
+      <div class="ui-block-d">
+        <h6>Information</h6>
+        <ul>
+          <li>Terms & Conditions</li>
+          <li>Cookie Consent</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+    </div><!-- /footer-content-grid -->
     <div class="copyright">
       <p>&copy; Copyright
         <script type="text/javascript">
