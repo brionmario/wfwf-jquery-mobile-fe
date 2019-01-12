a12c7a9649710e8461f48d760e42790631d1d8ed
Ravintheja <ravinhett11@gmail.com>
2019-01-12 11:37:25 +0530

    favorites page without styling

---
+++ src/favorites.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html>
+</<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="author" content="Brion Silva">
+  <title>jQuery Mobile Seed by Aparecium Labs</title>
+  <!-- Favicon Package -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
+  <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
+  <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+  <!-- inject:css -->
+  <!-- endinject -->
+
+    <!-- inject:js -->
+    <!-- endinject -->
+</head>
+<body>
+
+<div data-role="page">
+
+	<div data-role="container" >
+	
+	<ul style="list-style-type:none">
+        <li>
+        <div class="items">
+            <div>
+			<table width="100%">
+				<tr>
+					<td width="30%"><img src="assets/img/img2.jpg" alt="dress" height="110" width="110"></td>
+					<td width="70%" id="space"><a href=#><h5> Armani Dress </h5></a>
+					<i><p> Price: $210
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+				<tr>
+					<td width="30%"><img src="assets/img/img2.jpg" alt="event" height="110" width="110"></td>
+					<td width="70%" id="space"><a href=#><h5> Giordano Catwalk </h5></a>
+					<i><p> In 3 days
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+				<tr>
+					<td width="30%"><img src="assets/img/img2.jpg" alt="team" height="110" width="110"></td>
+					<td width="70%" id="space"><a href=#><h5> Brion Silva </h5></a>
+					<i><p> Team Leader
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+				<tr>
+					<td width="30%"><img src="assets/img/img2.jpg" alt="jacket" height="110" width="110"></td>
+					<td width="70%" id="space"><a href=#><h5> Nike Jacket </h5></a>
+					<i><p> Price: $70
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+			</table>
+            </div>
+        </div>
+        </li>
+
+		</ul>
+	</div>
+</center>
+</div><!-- /page -->
+    
+</body>
+</html>
