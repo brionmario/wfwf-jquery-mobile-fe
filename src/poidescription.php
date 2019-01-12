dca4b24af1266bba53faa2019dd1c59022778ab5
Ravintheja <ravinhett11@gmail.com>
2019-01-12 14:17:18 +0530

    Poi list and description

---
+++ src/poidescription.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="author" content="Brion Silva">
+  <title>jQuery Mobile Seed by Aparecium Labs</title>
+ 
+  <!-- Favicon Package -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
+  <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
+  <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+  <!-- inject:css -->
+  <!-- endinject -->
+
+  <!-- inject:js -->
+  <!-- endinject -->
+</head>
+
+<body>
+  <div data-role="page"> 
+    <div>   
+    <div data-role="popup" id="popupNYC" data-overlay-theme="b" data-theme="b" data-corners="false">
+        <img class="popphoto" src="assets/img/img3.jpg" height="200px" width="250px" alt="eye">
+    </div>
+    <div> 
+        <h3> London Eye</h3>
+        <p> The London Eye is a cantilevered observation wheel on the South Bank of the River Thames 
+            in London. It is Europe's tallest cantilevered observation wheel, is the most popular paid 
+            tourist attraction in the United Kingdom with over 3.75 million visitors annually, and has 
+            made many appearances in popular culture. </p>
+    </div>
+    <div>
+        <table>
+            <tr><td width="89%" padding="5px">
+                <form>
+                    <input class="btn btn-primary" type="button" value="Get Directions">
+                </form>
+                </td>
+                <td>
+                    <button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+                </td>
+            </tr>       
+        </table>
+    </div>
+  </div>
+
+</body>
+</html>
---
+++ src/poilist.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html>
+</<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+  <meta name="author" content="Brion Silva">
+  <title>jQuery Mobile Seed by Aparecium Labs</title>
+  <!-- Favicon Package -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
+  <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
+  <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+  <!-- inject:css -->
+  <!-- endinject -->
+
+    <!-- inject:js -->
+    <!-- endinject -->
+</head>
+<body>
+
+<div data-role="page">
+
+	<div data-role="container" >
+	
+	<ul style="list-style-type:none">
+        <li>
+        <div class="items">
+            <div>
+			<table width="100%">
+				
+                <tr>
+					<td width="30%"><img src="assets/img/img3.jpg" alt="dress" height="100" width="125"></td>
+					<td width="70%" id="space"><a href=#><h5> Hyde Park </h5></a>
+					<i><p> 2 miles away
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+                </tr>
+
+                <tr>
+					<td width="30%"><img src="assets/img/img3.jpg" alt="dress" height="100" width="125"></td>
+					<td width="70%" id="space"><a href=#><h5> Palace of Westminister </h5></a>
+					<i><p> 3 miles away
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+                </tr>
+
+                <tr>
+					<td width="30%"><img src="assets/img/img3.jpg" alt="dress" height="100" width="125"></td>
+					<td width="70%" id="space"><a href=#><h5> Big Ben </h5></a>
+					<i><p> 3 miles away
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+                </tr>
+
+				<tr>
+					<td width="30%"><img src="assets/img/img3.jpg" alt="event" height="100" width="125"></td>
+					<td width="70%" id="space"><a href=#><h5> Buckingham Palace </h5></a>
+					<i><p> 4 miles away
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+				<tr>
+					<td width="30%"><img src="assets/img/img3.jpg" alt="team" height="100" width="125"></td>
+					<td width="70%" id="space"><a href=#><h5> University of Westminister </h5></a>
+					<i><p> 1 miles away
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+				<tr>
+					<td width="30%"><img src="assets/img/img3.jpg" alt="jacket" height="100" width="125"></td>
+					<td width="70%" id="space"><a href=#><h5> London Eye </h5></a>
+					<i><p> 2 miles away
+					<button class="ui-btn ui-shadow ui-corner-all ui-icon-heart ui-btn-icon-notext"></button>
+					</p></td>
+				</tr>
+
+			</table>
+            </div>
+        </div>
+        </li>
+
+		</ul>
+	</div>
+</center>
+</div><!-- /page -->
+    
+</body>
+</html>
