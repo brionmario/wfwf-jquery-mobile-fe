51084a40c076df735a4afa41cadbb48c0abafaf5
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-13 02:53:33 +0530

    feat(auth): add logic and sign up page mark up :sparkles:

--- src/login.php
+++ src/login.php
@@ -7,7 +7,8 @@
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="theme-color" content="#ffffff">
   <meta name="author" content="Thisura Sagara">
-  <title>Login</title>
+  <title>Login | Westminster Fashion Week Festival 2019</title>
+  
   <!-- Favicon Package -->
   <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
@@ -17,53 +18,54 @@
   <!-- inject:css -->
   <!-- endinject -->
 
-    <!-- inject:js -->
+  <!-- inject:js -->
   <!-- endinject -->
 </head>
 
 <body>
-<div data-role="page" class="auth">
-  <div role="main" class="overlay ui-content main-content">
-  <div class="close-icon">
-  <i class="fa fa-times fa-2x"></i>
+<div data-role="page">
+  <div role="main" class="overlay ui-content main-content auth-page">
+    <div class="close-icon-container">
+      <i class="fa fa-times fa-2x"></i>
     </div>
-    <div class="main-heading-area">
-      <h1>Login</h1>
-    </div>
-    <div class="second-heading-area">
-      <h3>Welcome Back,</h3>
-    </div>
-    <div class="third-heading-area">
-      <h5>Sign in to continue</h5>
-    </div>
-    <div class="form-area">
-      <div class="inputWithIcon">
-           <input type="email" name="email" placeholder="Email">
-           <i class="fa fa-envelope fa-lg"></i>
+    <div class="content">
+      <div class="main-heading-container">
+        <h1>Login</h1>
+        <h3>Welcome Back,</h3>
+        <h5>Sign in to continue</h5>
+      </div>
+      <div class="form-area">
+        <div class="input-with-icon">
+          <input type="email" name="email" placeholder="Email">
+          <i class="fa fa-envelope"></i>
         </div>
-        <div class="inputWithIcon">  
+        <div class="input-with-icon">  
           <input type="password" name="password" placeholder="Password">
-          <i class="fa fa-lock fa-lg"></i>
+          <i class="fa fa-lock"></i>
         </div>
         <div class="form-button">
-          <button type="button" onclick="alert('Login')">Login</button>
+          <button type="submit" class="btn btn-primary" onclick="alert('Login')">Login</button>
         </div>
-    </div>
-    <div class="route-area">        
-     <p>Don't have an account? click <a href="sign-up.php">here</a> to sign up new !</p>
-    </div>
-    <hr class="hr-text" data-content="OR">
-    <div class="sign-with-heading-area">
-      <h5>Sign in with</h5>
-    </div>
-      <div class="social-icons">
-        <ul>
-          <li><a href="https://www.facebook.com/apareciumlabs/" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
-          <li><a href="https://twitter.com/apareciumlabs" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
-          <li><a href="https://instagram.com/apareciumlabs" target="_blank"><i class="fa fa-google"></i></a></li>
-        </ul>
       </div>
-  </div><!-- /content -->
+      
+      <div class="route-area">        
+        <p>Don't have an account? Click <a href="sign-up.php">here</a> to sign up now!</p>
+      </div>
+
+      <hr class="hr-text" data-content="OR">
+
+      <div class="social-login-container">
+        <h5>Sign in with</h5>
+        <div class="social-icons">
+          <ul>
+            <li><a href="https://www.facebook.com/apareciumlabs/" target="_blank"><i class="fa fa-facebook-square"></i></a></li>
+            <li><a href="https://twitter.com/apareciumlabs" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
+            <li><a href="https://instagram.com/apareciumlabs" target="_blank"><i class="fa fa-google-plus-square"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div><!-- /content -->
+  </div><!-- /main-content -->
 </div><!-- page -->
 </body>
 </html>
--- src/sign-up.php
+++ src/sign-up.php
@@ -7,61 +7,60 @@
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="theme-color" content="#ffffff">
   <meta name="author" content="Thisura Sagara">
-  <title>Sign Up</title>
+  <title>Sign Up | Westminster Fashion Week Festival 2019</title>
   <!-- Favicon Package -->
   <link rel="apple-touch-icon" sizes="180x180" href="./assets/icons/favicon_package/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="./assets/icons/favicon_package/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="./assets/icons/favicon_package/favicon-16x16.png">
   <link rel="manifest" href="./assets/icons/favicon_package/site.webmanifest">
   <link rel="mask-icon" href="./assets/icons/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+
   <!-- inject:css -->
   <!-- endinject -->
 
-    <!-- inject:js -->
+  <!-- inject:js -->
   <!-- endinject -->
 </head>
 
 <body>
-<div data-role="page" class="auth">
-  <div role="main" class="overlay ui-content main-content">
-  <div class="close-icon">
-  <i class="fa fa-times fa-2x"></i>
+<div data-role="page">
+  <div role="main" class="overlay ui-content main-content auth-page">
+    <div class="close-icon-container">
+      <i class="fa fa-times fa-x"></i>
     </div>
-    <div class="main-heading-area">
-      <h1>Sign up</h1>
-    </div>
-    <div class="second-heading-area">
-      <h3>Howdy stranger,</h3>
-    </div>
-    <div class="third-heading-area">
-      <h5>Complete the form to create an account</h5>
-    </div>
-    <div class="form-area">
-          <div class="inputWithIcon">
-            <input type="text" name="fname" placeholder="First Name">
-            <i class="fa fa-user fa-lg"></i>
-          </div>
-         <div class="inputWithIcon">  
-         <input type="text" name="lname" placeholder="Last Name">
-         <i class="fa fa-user fa-lg"></i>
-         </div>
-           <div class="inputWithIcon"> 
-           <input type="email" name="email" placeholder="Email">
-           <i class="fa fa-envelope fa-lg"></i>
-           </div>
-          <div class="inputWithIcon">  
+    <div class="content">
+      <div class="main-heading-container">
+        <h1>Sign up</h1>
+        <h3>Howdy stranger,</h3>
+        <h5>Complete the form to create an account</h5>
+      </div>
+      <div class="form-area">
+        <div class="input-with-icon">
+          <input type="text" name="fname" placeholder="First Name">
+          <i class="fa fa-user"></i>
+        </div>
+        <div class="input-with-icon">  
+          <input type="text" name="lname" placeholder="Last Name">
+          <i class="fa fa-user"></i>
+        </div>
+        <div class="input-with-icon"> 
+          <input type="email" name="email" placeholder="Email">
+          <i class="fa fa-envelope"></i>
+        </div>
+        <div class="input-with-icon">  
           <input type="password" name="password" placeholder="Password">
-          <i class="fa fa-lock fa-lg"></i>
-          </div>
-          <div class="form-button">
-            <button type="button" onclick="alert('Sign up')">Sign up</button>
-          </div>
-    </div>
+          <i class="fa fa-lock"></i>
+        </div>
+        <div class="form-button">
+          <button type="submit" class="btn btn-primary" onclick="alert('Sign up')">Sign up</button>
+        </div>
+      </div>
     
-    <div class="route-area">        
-     <p>Alredy have an account? Click <a href="login.php">here</a> to sign in!</p>
-    </div>
-  </div><!-- /content -->
+      <div class="route-area">        
+        <p>Alredy have an account? Click <a href="login.php">here</a> to sign in!</p>
+      </div>
+    </div><!-- /content -->
+  </div><!-- /main-content -->
 </div><!-- page -->
 </body>
 </html>
